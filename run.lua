cbedf577ebc199c810171ee0206f60bc8477f375
Alykhan Tejani <alykhan.tejani@gmail.com>
2015-07-06 19:11:30 +0100

    added the train function to train a model using SGD

---
+++ run.lua
@@ -0,0 +1,95 @@
+require 'torch'
+require 'nn'
+require 'optim'
+require 'image'
+require 'dataset'
+
+
+-----------------------------------------------------------------------------
+--------------------- parse command line options ----------------------------
+-----------------------------------------------------------------------------
+cmd = torch.CmdLine()
+cmd:text()
+cmd:text("Options")
+cmd:option("-gpu", false, "use gpu")
+cmd:option("-training_data", mnist.trainset_path, "training data (.t7) file")
+cmd:option("-log", "out.log", "output log file")
+cmd:option("-batch_size", 50, "batch size")
+cmd:option("-learning_rate", 0.001, "learning_rate")
+cmd:option("-snapshot_dir", "", "snapshot directory")
+
+params = cmd:parse(arg)
+
+logger = optim.Logger(params.log)
+training_data = mnist.load_normalized_dataset(params.training_data)
+
+epoch = 0
+batch_size = params.batch_size
+learning_rate = params.learning_rate
+
+
+--Load model and criterion
+
+-- retrieve parameters and gradients (Global)
+parameters, grad_parameters = model:getParameters()
+
+function train(dataset)
+      
+    local time = sys.clock()
+    --train one epoch of the dataset
+    print("training epoch #" .. epoch)
+   
+    for mini_batch_start = 1, dataset:size(), batch_size do 
+        xlua.progress(mini_batch, dataset:size()) --display progress
+
+        local inputs = {}
+        local labels = {}
+        --create a mini_batch
+        for i = mini_batch_start, math.min(mini_batch_start + batch_size - 1, dataset:size()) do 
+            local input = dataset[i][1]:clone() -- the tensor containing two images 
+            local label = dataset[i][2]:clone() -- +/- 1
+            table.insert(inputs, input)
+            table.insert(labels, label)
+        end
+
+        --create a closure to evaluate df/dX where X are the model parameters
+        local func_eval = 
+        function(x)
+                if x ~= parameters then
+                    parameters:copy(x) --update the model parameters (copy x in to parameters)
+                end
+
+                grad_parameters:zero() --reset gradients
+
+                local avg_error = 0 -- the average error of all criterion outs
+
+                --evaluate for complete mini_batch
+                for i = 1, #inputs do
+                    local output = model:forward(inputs[i])
+                    local err = criterion:forward(output, labels[i])
+                    avg_error = avg_error + err
+
+                    --estimate dLoss/dW
+                    local dloss_dout = criterion:backward(output, labels[i])
+                    model:backward(inputs[i], dloss_dout)
+                end
+
+                grad_parameters:div(#inputs)
+                avg_error = avg_error / #inputs
+
+                return avg_error, grad_parameters
+        end
+        config = {learningRate = opt.learningRate,
+                  weightDecay = opt.weightDecay,
+                  momentum = opt.momentum,
+                  learningRateDecay = 5e-7}
+        optim.sgd(func_eval, parameters, config)
+    end
+
+    -- time taken
+    time = sys.clock() - time
+    time = time / dataset:size()
+    print("time taken for 1 epoch = " .. (time * 1000) .. "ms, time taken to learn 1 sample = " .. ((time/dataset:size())*1000) .. 'ms')
+
+    epoch = epoch + 1
+end
